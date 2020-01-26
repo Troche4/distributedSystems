3785a3b0559923a2e509a93c27c9f4cf70ec63c0
Trey Roche <proche@luc.edu>
2020-01-26 15:52:41 -0600

    Implemented parsing and test case  XML

--- test/testParser.py
+++ test/testParser.py
@@ -1,1 +1,24 @@
+#Author: Trey Roche
+#Program designed to test XMLParser.py code
+#Resources used: unittest framework in Python libraries (docs.python.org)
 
+from unittest import *
+
+class testParser(unittest.case):
+
+    def testUnrecognizedElement():
+
+    def testNoQuestionType():
+
+    def testQuestions():
+
+    def testEmptyXML():
+
+    def testNoQuestions():
+
+    def testNoDirections():
+
+    def testNoTitle():
+
+if __name__ == "main":
+    unittest.main()
