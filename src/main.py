309f239d3e2f4b694d74c896e23cd7f1f499a684
Trey Roche <proche@luc.edu>
2020-02-04 10:05:32 -0600

    WIP client side and app skeleton

--- src/main.py
+++ src/main.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/student")
+def student():
+    return render_template("student.html")
+
+@app.route("/instructor")
+def instructor():
+    return render_template("instructor.html")
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+@app.route("/session")
+def session():
+    sessionCode = str()
+    for i in range(0,6):
+        sessionCode += str(random.randint(0,9))
+    return "The code for your session is:" sessionCode
+
 
 if __name__ == "__main__":
     app.run(debug=True)
